11b36d0e02727d4c42541ce5acec32b4a42a7574
Steve Gardner <gardnersj@gmail.com>
2025-05-30 19:37:48 +0100

    Update figure and tech-debt article

--- components/ui/figure.tsx
+++ components/ui/figure.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import { ImageProps } from 'next/image';
+import Link from 'next/link';
 
 interface FigureProps extends React.HTMLAttributes<HTMLDivElement> {
-  caption: React.ReactNode;
   children: React.ReactElement<ImageProps>;
+  creator?: {
+    name: string;
+    url: string;
+  };
+  source?: {
+    name: string;
+    url: string;
+  };
 }
 
-export function Figure({ caption, children, className, ...props }: FigureProps) {
+export function Figure({ creator, source, children, className, ...props }: FigureProps) {
   return (
     <figure
       className={`mt-10 mb-12 flex flex-col items-center ${className || ''}`}
       {...props}
     >
       {children}
-      <figcaption className="mt-2 text-center text-sm text-muted-foreground">
-        {caption}
-      </figcaption>
+      {creator?.url && source?.url && (
+        <figcaption className="mt-2 text-center text-sm text-muted-foreground">
+          Photo by{' '}
+          <Link
+            className="underline"
+            href={creator.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {creator.name}
+          </Link>{' '}
+          on{' '}
+          <Link
+            className="underline"
+            href={source.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {source.name}
+          </Link>
+        </figcaption>
+      )}
     </figure>
   );
 }
